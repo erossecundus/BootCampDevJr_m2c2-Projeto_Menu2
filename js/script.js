06050f517f68d0a8a60503a4656b749bd2013b77
Eros Secundus <erossecundus@gmail.com>
2024-04-14 04:59:13 -0300

    closes #5 - Implementação do Modal

--- js/script.js
+++ js/script.js
@@ -1,41 +1,48 @@
-$("#phone").mask("(99) 9 9999-9999");
+$("#inputPhone").mask("(99) 9 9999-9999");
 
 var prod = [
-    {id: 1, name: "Bife com batata", price: 30.0},
-    {id: 2, name: "Coxa de frango crocante", price: 25.0},
-    {id: 3, name: "Carne de panela", price: 22.0},
-    {id: 4, name: "Farofa", price: 10.0},
-    {id: 5, name: "Salada", price: 8.0},
-    {id: 6, name: "Torresmo", price: 12.0}
+    { id: 1, name: "Bife com batata", price: 30.0 },
+    { id: 2, name: "Coxa de frango crocante", price: 25.0 },
+    { id: 3, name: "Carne de panela", price: 22.0 },
+    { id: 4, name: "Farofa", price: 10.0 },
+    { id: 5, name: "Salada", price: 8.0 },
+    { id: 6, name: "Torresmo", price: 12.0 }
 ]
 
 function calc() {
     var quantities  = document.getElementsByName("quantity");
     var output      = document.getElementById("output");
     var total       = 0;
-    var clientName      = document.getElementById("name")
+    var clientName  = document.getElementById("inputNome");
+    var outputName  = document.getElementById('outputTitle');
+    var clientMsg   = document.getElementById("inputObs");
 
     var formatter = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
         currency: 'BRL',
     });
 
-    output.innerHTML = `<p>Caro <strong>${clientName.value}</strong></p>`;
-    output.innerHTML += `<p></br>Seguem os dados do seu pedido.<br><br>Seu pedido é:</p><ul>`
+    
+
+    outputName.innerHTML = `<h3>Caro <strong>${clientName.value}</strong></h3>`;
+    output.innerHTML = '';
+    output.innerHTML += `<p>Seguem os dados do seu pedido.</p>Seu pedido é:<ol>`
     
     for (var input of quantities) {
         if (parseFloat(input.value)){
-            output.innerHTML += `<li>Prato: ${prod[input.id-1].name} - 
-                                Preço unitário: ${formatter.format(prod[input.id-1].price)} - 
-                                Quantidade: ${input.value} - 
-                                Total: ${formatter.format(parseFloat(input.value)*prod[input.id-1].price)}`;
+            output.innerHTML += `<li class="fs-5">Prato: ${prod[input.id-1].name}</li>
+                                <ul>
+                                <li>Preço unitário: ${formatter.format(prod[input.id-1].price)} | 
+                                    Quant.: ${input.value} | Total parcial: 
+                                    <strong>${formatter.format(parseFloat(input.value) * prod[input.id-1].price)}</strong></li>
+                                </ul>`;
             total            += parseFloat(input.value)*prod[input.id-1].price;
         }
     }
-
-    output.innerHTML += `</ul><p></br><h2>Preço final: ${formatter.format(total)}</h2>`;
-
+    output.innerHTML += "</ol>" ;
+    if(clientMsg.value)
+        output.innerHTML += `<p class="fst-italic">Observações: ${clientMsg.value}</p>`;
+    output.innerHTML += `<h2 class="text-center">Preço final: ${formatter.format(total)}</h2>`;
     
-
 
 }
